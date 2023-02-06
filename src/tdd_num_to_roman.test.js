67155fa1bacd5ed6c408b547036d3506135ca80e
sandyatech <sandhya.mskgmail.com>
2023-02-06 12:19:19 +0000

    modified files names

---
+++ src/tdd_num_to_roman.test.js
@@ -0,0 +1,22 @@
+const numToRoman = require('./tdd_num_to_roman')
+
+test('convert numbers to roman numerals for 1-10', () => {
+    expect(numToRoman(1)).toEqual('I');
+    expect(numToRoman(5)).toEqual('V');
+    expect(numToRoman(9)).toEqual('IX')
+    expect(numToRoman(10)).toEqual('X');
+});
+
+test('convert numbers to roman numerals for 100s', () => {
+    expect(numToRoman(50)).toEqual('L');
+    expect(numToRoman(100)).toEqual('C');
+    expect(numToRoman(500)).toEqual('D');
+    expect(numToRoman(1000)).toEqual('M');
+});
+test('convert numbers to roman numerals for typical numbers', () => {
+    expect(numToRoman(900)).toEqual('CM');
+    expect(numToRoman(400)).toEqual('CD');
+    expect(numToRoman(90)).toEqual('XC');
+    expect(numToRoman(117)).toEqual('CXVII');
+    expect(numToRoman(786)).toEqual('DCCLXXXVI');
+});
